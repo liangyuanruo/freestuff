06bb45b5ae592309904e2c128c3c71f0a109f72c
Li Hongyi <iygnohil@gmail.com>
2023-01-18 16:06:10 +0000

    Basic styling for listing page

--- app/src/js/listing.js
+++ app/src/js/listing.js
@@ -1,10 +1,12 @@
-console.log("hello listing.js")
-
-
 const preview = document.getElementsByClassName('preview')[0]
+const fileChange = document.getElementsByClassName('change')[0]
 const fileInput = document.getElementById('file')
 fileInput.onchange = event => {
   const [file] = fileInput.files
   console.log(file)
-  if (file) preview.src = URL.createObjectURL(file)
+  if (file) {
+    preview.src = URL.createObjectURL(file)
+    preview.classList.remove("hidden")
+    fileChange.classList.remove("hidden")
+  }
 }
