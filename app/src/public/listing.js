5f6a58766e9e041f6bb9616ce8ad2045d195be14
Li Hongyi <iygnohil@gmail.com>
2023-01-18 14:04:53 +0000

    Image preview for upload listing

--- app/src/public/listing.js
+++ app/src/public/listing.js
@@ -1,1 +1,10 @@
 console.log("hello listing.js");
+
+
+const preview = document.getElementsByClassName('preview')[0];
+const fileInput = document.getElementById('file');
+fileInput.onchange = event => {
+  const [file] = fileInput.files;
+  console.log(file);
+  if (file) preview.src = URL.createObjectURL(file);
+};
