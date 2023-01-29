78bc41f90bec9766cc1fb657d43e92501cbb533d
Li Hongyi <iygnohil@gmail.com>
2023-01-29 15:25:20 +0000

    Refactored listing.js

--- app/src/js/listing.js
+++ app/src/js/listing.js
@@ -1,32 +1,43 @@
-// Disable the submit button by default
-const submit = document.querySelector('button[type=submit]')
-submit.disabled = true
+console.log('Start')
 
-// Enable the submit button only if all fields are filled
+console.log('Creating checks to enable submit button if all fields are filled')
 const form = document.querySelector('form')
 const fields = document.querySelectorAll('form input, form select')
-form.addEventListener('change', () => {
-  // If fields are an empty string, undefined, or null then disable the button
+const checkSubmit = () => {
+  console.log('checkSubmit: Start')
   for (const field of fields) {
+    // If fields are an empty string, undefined, or null then disable the button
     if (!field.value) {
+      console.log('checkSubmit: Some fields empty. Disabling submit button')
       submit.disabled = true
       return
     }
   }
-  // If all pass then enable submit
+  console.log('checkSubmit: All fields filled. Enabling submit button')
   submit.disabled = false
-})
+}
+checkSubmit()
+form.addEventListener('input', checkSubmit)
+form.addEventListener('change', checkSubmit)
+setTimeout(checkSubmit, 1000) // Timeout to check for browser autofill
 
-// Set auto lister 
+console.log('Creating checks to display image preview if file selected')
 const preview = document.getElementsByClassName('preview')[0]
 const fileChange = document.getElementsByClassName('change')[0]
 const fileInput = document.getElementById('file')
-fileInput.addEventListener("change", event => {
+const checkFile = () => {
+  console.log('checkFile: Start')
   const [file] = fileInput.files
-  console.log(file)
   if (file) {
+    console.log('checkFile: File selected. Displaying preview')
     preview.src = URL.createObjectURL(file)
     preview.classList.remove('hidden')
     fileChange.classList.remove('hidden')
+    return
   }
-})
+}
+checkFile()
+fileInput.addEventListener("change", checkFile)
+setTimeout(checkFile, 1000) // Timeout to check for browser autofill
+
+console.log("End")
